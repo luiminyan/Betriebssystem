3c577a8826f9a621120789a54d1a8418a6b0f4f8
Minh <94862685+luiminyan@users.noreply.github.com>
2023-07-20 13:30:47 +0200

    finish clash in Aufgaben

--- Aufgaben/A3_clash/clash.c
+++ Aufgaben/A3_clash/clash.c
@@ -62,14 +62,59 @@ static void prompt_symbol(){
 }
 
 static void collect_zombies() {
-    //TODO:collect all background zombie processes
-    
+    //collect all background zombie processes
+    while (1) {
+        //reset errno
+        errno = 0;
+        int status;
+        pid_t wpid = waitpid(-1, &status, WNOHANG); //do not block parent process to wait
+        if (wpid == -1) {
+            //no child to collect
+            if (errno == ECHILD) {
+                break;
+            }
+            //error by waitpid
+            else die("waitpid");
+        }
+        //child process still not terminated
+        else if (wpid == 0) {
+            continue;
+        }
+        
+        //zombie collected
+        //remove zombie process from list
+        //create buffer for "jobs"
+        char jobs[MAX_LINE + 1];    
+        if (removeElement(wpid, jobs, sizeof(jobs)) == -1) {
+            //process not in list
+            continue;
+        }
+        
+        //if process successfully exited
+        if (WIFEXITED(status)) {
+            printf("Exitstatus [%s] = %d\n", jobs, WEXITSTATUS(status));
+        }
+        else printf("Process[%s] does not successfully exited.\n", jobs);
+
+        //flush
+        if (fflush(stdout)) {
+            die("fflush");
+        }
+    }
+}
+
+static bool print_jobs(pid_t pid, const char* jobs){
+    if (printf("pid: %d, jobs: %s\n", pid, jobs) < 0) {
+        die("printf");
+    }
+    return 0;
 }
 
 int main (int argc, char* argv[]) {
     while (1) {
-        //TODO: collect zombies (back ground processes)
+        //collect zombies (back ground processes)
         collect_zombies();
+
         //background
         int isBackground = 0; 
 
@@ -87,7 +132,7 @@ int main (int argc, char* argv[]) {
                 die("fgets");
             }
             //empty line, user input: ctrl + d...
-            else continue;
+            else break;
         }
 
         //regular input modify
@@ -156,7 +201,29 @@ int main (int argc, char* argv[]) {
         printf("sum of parameters: %d\n", index);
         for (int j = 0; j <= index; j++) {
             printf("%s\n", arg_list[j]);
-        } */      
+        } */ 
+
+        //cmd = "cd"
+        if (strcmp(arg_list[0], "cd") == 0) {
+            if (index < 2 || index >= 3 || arg_list[1] == NULL) {
+                warn("too less|much arguments\n");
+                continue;
+            }
+            if (chdir(arg_list[1]) != 0) {
+                warn("chdir");
+            }
+        }     
+
+        //cmd = "jobs"
+        if (strcmp(arg_list[0], "jobs") == 0) {
+            if (arg_list[1] != NULL) {
+                warn("too much arguments");
+                continue;
+            }
+            //do the walklist
+            walkList(print_jobs);
+            
+        }
 
         //create child process 
         pid_t pid = fork();
@@ -177,9 +244,11 @@ int main (int argc, char* argv[]) {
         //if process in background
         if (isBackground) {
             //do not wait here
-            //TODO: use the list in plist.c
-            
-           
+            //TODO: add the child process in the list in plist.c
+            if (insertElement(pid, job) == -2) {
+                warn("malloc / strdup");
+                exit(EXIT_FAILURE);
+            }
         }
         else {
             //if process in foreground
